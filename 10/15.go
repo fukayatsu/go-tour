812b9e3e6a6e649eb3bf75c82353cc4ddefdd708
fukayatsu <fukayatsu@gmail.com>
2014-01-02 23:45:23 +0900

    [from now] 2014/01/02 23:45:23

---
+++ 10/15.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	Pi = 3.14
+)
+
+func main() {
+	const (
+		World = "世界"
+		Truth = true
+	)
+
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	fmt.Println("Go rules?", Truth)
+}
