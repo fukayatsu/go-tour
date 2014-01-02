1b2288a992f54481d885647f759bb580a32794ee
fukayatsu <fukayatsu@gmail.com>
2014-01-02 19:32:21 +0900

    [from now] 2014/01/02 19:32:21
    diff --git a/7.go b/7.go
    index 79dec66..900bc27 100644
    --- a/7.go
    +++ b/7.go
    @@ -4,10 +4,11 @@ import (
     	"fmt"
     )
    
    -func add(x, y int) int {
    +func add(x, y, z int, s string, a int) int {
    +	fmt.Println(a, s)
     	return x + y
     }
    
     func main() {
    -	fmt.Println(add(42, 13))
    +	fmt.Println(add(42, 13, 55, "foo", 123))
     }

--- 7.go
+++ 7.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 )
 
-func add(x, y int) int {
+func add(x, y, z int, s string, a int) int {
+	fmt.Println(a, s)
 	return x + y
 }
 
 func main() {
-	fmt.Println(add(42, 13))
+	fmt.Println(add(42, 13, 55, "foo", 123))
 }
